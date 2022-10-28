1433676912e68b89911162e8cd2daf21957986ec
nebojsakl <nebojsa.klasanovic@dev.telnet.rs>
2022-10-28 14:46:30 +0200

    Practice of Angular

--- routing/js/script.js
+++ routing/js/script.js
@@ -1,31 +1,52 @@
 
- angular.module("demo", ["ngRoute"])
-    .config(function ($routeProvider){
+ var app = angular
 
-        $routeProvider
+    .module("demo", ["ui.router"])
+    .config(function ($stateProvider, $urlMatcherFactoryProvider, $urlRouterProvider, $locationProvider){
+
+        $urlRouterProvider.otherwise("/home");
+        $urlMatcherFactoryProvider.caseInsensitive(true);
+        $stateProvider
     
-        .when("/home", {
+        .state("home", {
+            url:"/home",
             templateUrl: "templates/home.html",
-            controller:"homeController as homeCtrl"
+            controller:"homeController as homeCtrl",
+            data : {
+                customData1 : "Home state custom data 1",
+                customData2 : "Home state custom data 2"
+            }
         })
 
-        .when("/courses", {
+        .state("courses", {
+            url:"/courses",
             templateUrl: "templates/courses.html",
-            controller:"coursesController as coursesCtrl"
+            controller:"coursesController as coursesCtrl",
+            data : {
+                customData1 : "Courses state custom data 1",
+                customData2 : "Courses state custom data 2"
+            }
         })
 
-        .when("/students", {
+        .state("students", {
+            url:"/students", 
             templateUrl: "templates/students.html",
             controller:"studentsController as studentsCtrl"
         })
 
-        
+        $locationProvider.html5Mode(true);
 
     })
 
 
-    .controller("homeController", function(){
+    .controller("homeController", function($state){
         this.message = "Home Page";
+
+        this.homeCustomData1 = $state.current.data.customData1;
+        this.homeCustomData2 = $state.current.data.customData2;
+
+        this.coursesCustomData1 = $state.get("courses").data.customData1;
+        this.coursesCustomData2 = $state.get("courses").data.customData2;
     })
 
     .controller("coursesController", function(){
